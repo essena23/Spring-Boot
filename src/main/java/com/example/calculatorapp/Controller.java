17e2618e01f3fa26d96f70808d096bcad04bab47
User <essena23@gmail.com>
2024-05-05 15:58:28 -0400

    2nd commit

--- src/main/java/com/example/calculatorapp/Controller.java
+++ src/main/java/com/example/calculatorapp/Controller.java
@@ -11,39 +11,45 @@ public class Controller {
     public Controller(Service service) {
         this.service = service;
     }
+
     @GetMapping
     public String hello() {
         return service.hello();
     }
+
     @GetMapping(path = "/calculator")
     public String calculator() {
         return service.calculator();
     }
+
     @GetMapping(path = "/calculator/plus")
     public String plusSign(@RequestParam(required = false, name = "num1") Integer num1, @RequestParam(required = false, name = "num2") Integer num2) {
         if (num1 == null || num2 == null) {
-            return "Ошибка! Введите параметр";
+            return "Ошибка! Введите параметр!";
         }
         return service.plusSign(num1, num2);
     }
+
     @GetMapping(path = "/calculator/minus")
     public String minusSign(@RequestParam(required = false, name = "num1") Integer num1, @RequestParam(required = false, name = "num2") Integer num2) {
         if (num1 == null || num2 == null) {
-            return "Ошибка! Введите параметр";
+            return "Ошибка! Введите параметр!";
         }
         return service.minusSign(num1, num2);
     }
+
     @GetMapping(path = "/calculator/multiply")
     public String multiplySign(@RequestParam(required = false, name = "num1") Integer num1, @RequestParam(required = false, name = "num2") Integer num2) {
         if (num1 == null || num2 == null) {
-            return "Ошибка! Введите параметр";
+            return "Ошибка! Введите параметр!";
         }
         return service.multiplySign(num1, num2);
     }
+
     @GetMapping(path = "/calculator/divide")
-    public String divideSign(@RequestParam(required = false, name = "num1") Integer num1, @RequestParam(required = false, name = "num2")  Integer num2) {
+    public String divideSign(@RequestParam(required = false, name = "num1") Integer num1, @RequestParam(required = false, name = "num2") Integer num2) {
         if (num1 == null || num2 == null) {
-            return "Ошибка! Введите параметр";
+            return "Ошибка! Введите параметр!";
         }
         if (num2 == 0) {
             return "Ошибка! На ноль делить нельзя!";
